bad97df2bae7292f0a365f4c0685663edbe7a114
Pedro Giacometto <pedro.giacometto@gmail.com>
2016-03-25 23:01:52 -0430

    add phpunit App

---
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class IndexControllerTest extends AbstractHttpControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+               
+                include 'config/application.config.php'
+        );
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/test');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('Test');
+        $this->assertControllerName('Test\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('test');
+    }
+
+}
